91e4aa9ad5dff7e5b3e3ea0863914458b02cfa93
SimonVillalonIT <simonvillalon10@gmail.com>
2024-03-09 01:55:58 -0300

    :sparkles: Hero finished
    Hero of the page finished

---
+++ src/scripts/navbar.ts
@@ -0,0 +1,12 @@
+const openNavbar = document.getElementById("open");
+const closeNavbar = document.getElementById("close");
+const menu = document.getElementById("menu")
+
+function toggle() {
+  openNavbar?.classList.toggle("hidden");
+  closeNavbar?.classList.toggle("hidden");
+  menu?.classList.toggle("translate-x-[100vw]");
+}
+
+openNavbar?.addEventListener("click", toggle);
+closeNavbar?.addEventListener("click", toggle);
